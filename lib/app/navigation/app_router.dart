17ec0c517e80cc525d36bc488a73cdc28e20746a
Ersin Aksar <ersinaksar@yandex.com>
2024-12-18 18:09:42 +0300

    feat(router): implement GoRouter for navigation management
    - Integrated GoRouter for managing navigation routes.
    - Added routes for home, counter, settings, and profile pages.
    - New file: app_router.dart
    - Updated: route_constants.dart

--- lib/app/navigation/app_router.dart
+++ lib/app/navigation/app_router.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'routes.dart';
+import '../../features/counter/presentation/pages/counter_page.dart';
+import '../../features/home/presentation/pages/home_page.dart';
+import '../../features/profile/presentation/pages/profile_page.dart';
+import '../../features/settings/presentation/pages/settings_page.dart';
+import 'route_constants.dart';
+
+/// TODO  import other pages
+
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/',
-  routes: appRoutes,
+  initialLocation: RouteConstants.homePage,
+  routes: [
+    GoRoute(
+      path: RouteConstants.homePage,
+      name: 'home',
+      builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      path: RouteConstants.counterPage,
+      name: 'counter',
+      builder: (context, state) => const CounterPage(),
+    ),
+    GoRoute(
+      path: RouteConstants.settingsPage,
+      name: 'settings',
+      builder: (context, state) => const SettingsPage(),
+    ),
+    GoRoute(
+      path: RouteConstants.profilePage,
+      name: 'profile',
+      builder: (context, state) => const ProfilePage(),
+    ),
+
+    /// TODO other routes
+  ],
 );
---
+++ lib/app/navigation/route_constants.dart
@@ -0,0 +1,8 @@
+class RouteConstants {
+  static const String homePage = '/';
+  static const String counterPage = '/counter';
+  static const String settingsPage = '/settings';
+  static const String profilePage = '/profile';
+
+  /// TODO  add other route names here
+}
